bc97da3544493bc499e7124937e0d5440fd5a928
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-29 10:44:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
+function palindrome(str) {
+            const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+            const reversedStr = cleanedStr.split('').reverse().join('');
+            return cleanedStr === reversedStr;
+        }
 
-function palindrome(str){
+        document.getElementById('checkPalindrome').addEventListener('click', function () {
+            const input = document.getElementById('inputText').value;
+            const isPalindrome = palindrome(input);
+            document.getElementById('result').textContent = isPalindrome ? 'It is a palindrome!' : 'It is not a palindrome.';
+        });
 
-}
 module.exports = palindrome
